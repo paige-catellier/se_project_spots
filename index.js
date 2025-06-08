87fef816a027f0a25864bdf10b6e1cf599125f3c
paige-catellier <paigemarie454@yahoo.com>
2025-06-08 09:14:35 -0400

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-const editProfileBtn = document.querySelector(".profile__edit-btn");
-const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");

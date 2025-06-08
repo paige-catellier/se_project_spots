bebad6f171e71befa179f220efcadf36f83c3331
paige-catellier <paigemarie454@yahoo.com>
2025-06-08 09:15:00 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,3 @@
-
+const editProfileBtn = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");

b1ab0278925ef0e30c3b08481b6c2174b7c1895b
paigecatellier <paigemarie454@yahoo.com>
2025-10-17 07:30:57 -0400

    update 4

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -4,11 +4,39 @@ class Api {
     this._headers = headers;
   }
 
+  getAppInfo() {
+    return Promise.all([this.getInitialCards()]);
+  }
+
   getInitialCards() {
     return fetch(`${this._baseUrl}/cards`, {
       headers: this._headers,
-    }).then((res) => res.json());
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
   }
+
+  editUserInfo({ name, about }) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  // REQUESTS create another method, getUserInfo (similar to getintitialcards,
+  // different base url), call in it in getappinfo array
 }
 
 export default Api;

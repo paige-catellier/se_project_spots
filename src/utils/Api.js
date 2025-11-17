107f4cdcacb5a2e5482907feb673c9f095c63f62
paigecatellier <paigemarie454@yahoo.com>
2025-11-17 16:23:18 -0500

    updates pt 11

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -5,7 +5,7 @@ class Api {
   }
 
   getAppInfo() {
-    return Promise.all([this.getInitialCards()]);
+    return Promise.all([this.getInitialCards(), this.getUserInfo()]);
   }
 
   getInitialCards() {
@@ -15,7 +15,19 @@ class Api {
       if (res.ok) {
         return res.json();
       }
-      return Promise.reject(`Error: ${res.status}`);
+      Promise.reject(`Error: ${res.status}`);
+    });
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      Promise.reject(`Error: ${res.status}`);
     });
   }
 
